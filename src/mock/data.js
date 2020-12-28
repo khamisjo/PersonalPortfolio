9a1e73177ff88ee9d52e258550fa52a26a3806d9
Jonathan Khamis <jonathan.khamis95@gmail.com>
2020-12-27 17:34:40 -0800

    update

--- src/mock/data.js
+++ src/mock/data.js
@@ -27,7 +27,7 @@ export const aboutData = {
   paragraphThree:
     '' +
     'To tell you a bit about my non-technical side, I like reading psychology and history books; psychology because I believe in the power of human mind and how important to learn how to leverage such a powerful tool to achieve our full potential, history because learning about the past helps us to understand the present and better predict the future. I also like to workout, play the guitar, cook and learn new food recipes, and spend time with family and friends.',
-  resume: 'https://www.dropbox.com/s/kry5ffaox036yba/Resume%20-%20Jonathan%20Khamis.pdf?dl=0', // if no resume, the button will not show up
+  resume: 'https://www.dropbox.com/s/7b8ekqc6k8wf7i9/Resume%20-%20Jonathan%20Khamis.pdf?dl=0', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -49,19 +49,19 @@ export const projectsData = [
     img: 'eventify.png',
     title: 'Eventify (web)',
     info:
-      'This is the web version of Eventify, and it is built for a class project to explore different scalability techniques.' +
-      'In this project, I was exposed to topics such as concurrency, caching, load balancing, and obserbility',
+      'Eventify is a web app that lets users post events and parties and check for upcoming ones based on their preferences.' +
+      'This app aims to connect students more to the student organizations and Greek Life in UCLA',
     info2: 'Built In: React, TypeScript, GraphQL, MySQL, Docker',
     url: 'https://github.com/scalableinternetservices/pdjd',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'comingsoon.jpg',
+    img: 'chiTinder.png',
     title: 'Chicken Tinder',
     info:
       'Chicken Tinder is an interactive web app that helps friends who cannot decide on a restaurant to finally find a match everybody agrees on. Inspired by Tinder’s design to find date matches, Chicken Tinder also lets users swipe left and right on restaurants instead of potential dates until they find a matched restaurant.',
-    info2: 'Built In: React, Node.js, Firebase, Yelp API  ',
+    info2: 'Built In: React, Node.js, Redis  ',
     url: 'https://github.com/sdulaney/UCLA-CS-130-Group4',
     repo: '', // if no repo, the button will not show up
   },
